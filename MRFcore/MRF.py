ce459654e8f680b6749a97a1c6d70ffdcdaf91c3
Wenchen Lie <438171766@qq.com>
2024-04-09 14:56:36 +0800

    优化代码

--- MRFcore/MRF.py
+++ MRFcore/MRF.py
@@ -32,12 +32,7 @@ logger.add(
     format="<green>{time:YYYY-MM-DD HH:mm:ss}</green> <red>|</red> <level>{level}</level> <red>|</red> <level>{message}</level>",
     level="DEBUG"
 )
-logger.remove()
-logger.add(
-    sink=sys.stdout,
-    format="<green>{time:YYYY-MM-DD HH:mm:ss}</green> <red>|</red> <level>{level}</level> <red>|</red> <level>{message}</level>",
-    level="DEBUG"
-)
+
 
 class MRF:
     # 注：
